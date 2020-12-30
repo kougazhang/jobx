24649a69a2fe857d9b9e0368b5292a425d44d10e
zhao.zhang <zhao.zhang@upai.com>
2020-12-30 11:34:15 +0800

    allow relation empty

--- job/trigger.go
+++ job/trigger.go
@@ -1,51 +1,51 @@
 package job
 
 import (
-	"fmt"
-	"github.com/pkg/errors"
-	"github.com/kougazhang/jobx/trigger"
+    "fmt"
+    "github.com/kougazhang/jobx/trigger"
+    "github.com/pkg/errors"
 )
 
 const (
-	TriggerRelationAnd = "and"
-	TriggerRelationOr  = "or"
+    TriggerRelationAnd = "and"
+    TriggerRelationOr  = "or"
 )
 
 type Trigger struct {
-	Conditions []trigger.Trigger
-	Relation   string
+    Conditions []trigger.Trigger
+    Relation   string
 }
 
 func (t Trigger) IsValidRelation() bool {
-	return t.Relation == TriggerRelationAnd || t.Relation == TriggerRelationOr
+    return t.Relation == TriggerRelationAnd || t.Relation == TriggerRelationOr
 }
 
 func (t Trigger) paincInvalidRelation() {
-	panic(fmt.Sprintf("trigger relation %s invalid", t.Relation))
+    panic(fmt.Sprintf("trigger relation %s invalid", t.Relation))
 }
 
 func (t Trigger) Trigger() (can bool, err error) {
-	if !t.IsValidRelation() {
-		t.paincInvalidRelation()
-	}
+    if len(t.Conditions) > 1 && !t.IsValidRelation() {
+        t.paincInvalidRelation()
+    }
 
-	for _, conditionFn := range t.Conditions {
-		can, err = conditionFn.CanStart()
-		if err != nil {
-			return can, errors.Wrap(err, "Filter")
-		}
-		if t.Relation == TriggerRelationAnd {
-			if !can {
-				return can, nil
-			}
-		} else if t.Relation == TriggerRelationOr {
-			if can {
-				return can, nil
-			}
-		} else {
-			t.paincInvalidRelation()
-		}
-	}
+    for _, conditionFn := range t.Conditions {
+        can, err = conditionFn.CanStart()
+        if err != nil {
+            return can, errors.Wrap(err, "Filter")
+        }
+        if t.Relation == TriggerRelationAnd {
+            if !can {
+                return can, nil
+            }
+        } else if t.Relation == TriggerRelationOr {
+            if can {
+                return can, nil
+            }
+        } else {
+            return can, err
+        }
+    }
 
-	return can, err
+    return can, err
 }
